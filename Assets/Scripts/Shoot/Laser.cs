cd88f3490ac1239811d41f5b7e407d982e3a0a33
p.luneau <paul.luneau@mail.com>
2024-12-11 19:28:18 +0100

    Prevented the laser from being able to damage the same enemy multiple times

--- Assets/Scripts/Shoot/Laser.cs
+++ Assets/Scripts/Shoot/Laser.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Collider2D))]
@@ -5,12 +7,22 @@ public class Laser : MonoBehaviour
 {
     [SerializeField] private int m_damageAmout;
 
+    [NonSerialized] private HashSet<Enemy> m_touchedEnemies;
 
+    public void ResetLaser()
+    {
+        m_touchedEnemies ??= new HashSet<Enemy>();
+        m_touchedEnemies.Clear();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.TryGetComponent(out Enemy enemy))
         {
+            if (!m_touchedEnemies.Add(enemy))
+            {
+                return;
+            }
             enemy.TakeDamage(m_damageAmout, ProjectileDamageType.Both);
         }
     }
